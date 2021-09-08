0c659928fc87ea94f63de865ad1d4db2d6648708
eduperottoni <edu.perottoni@gmail.com>
2021-09-08 20:17:02 -0300

    Trabalhando com a dinâmica de abertura e fechamento do menu lateral

---
+++ assets/scripts/script.js
@@ -0,0 +1,7 @@
+const menuButton = document.getElementById('menu-button')
+const menuMobile = document.querySelector('nav#mobile')
+console.log(menuMobile)
+
+function openMenu(){
+    menuMobile.style.display='flex'
+}
