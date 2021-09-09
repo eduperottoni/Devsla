5b3959d2d2e0fa5424fa2d4b3544584b95b167fc
eduperottoni <edu.perottoni@gmail.com>
2021-09-09 00:21:09 -0300

    Finalizando script e animações de abertura do menu lateral

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,7 +1,40 @@
-const menuButton = document.getElementById('menu-button')
+const openMenuButton = document.getElementById('menu-button')
+const closeMenuButton = document.getElementById('exit')
 const menuMobile = document.querySelector('nav#mobile')
-console.log(menuMobile)
+const bcgMobile = document.querySelector('.bcg-mobile')
+const blurList = Array.from(document.querySelectorAll('.blur'))
 
-function openMenu(){
-    menuMobile.style.display='flex'
+console.log(blurList)
+
+function addBlur(array){
+    array.map((item)=>{
+        item.classList.add('onblur')
+        item.classList.remove('outblur')
+    })
 }
+
+function removeBlur(array){
+    array.map((item)=>{
+        item.classList.remove('onblur')
+        item.classList.add('outblur')
+    })
+}
+
+openMenuButton.addEventListener('click', (e) => {
+    e.preventDefault()
+    menuMobile.classList.remove('close')
+    menuMobile.classList.add('open')
+    bcgMobile.classList.remove('close')
+    bcgMobile.classList.add('open')
+    addBlur(blurList)
+})
+
+closeMenuButton.addEventListener('click', (e) => {
+    e.preventDefault()
+    menuMobile.classList.remove('open')
+    menuMobile.classList.add('close')
+    bcgMobile.classList.add('close')
+    bcgMobile.classList.remove('open')
+    removeBlur(blurList)
+})
+
